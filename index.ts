3494581c1ff90852f086889c3ca8c857498cb907
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-30 22:19:37 -0300

    add type Pessoa and Shcema

--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-console.log('teste')
+import { IPessoa } from "./index.d"
+import { connection, Schema, connect, model} from 'mongoose';
+
+
+connect(
+    'mongodb://localhost/test',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+const db = connection;
+
+db.on('error', console.error.bind(console, 'connection error'));
+
+db.on('open', () => {
+    console.log('Estamos conectados ao MongoDB!')
+})
+
+const pessoaSchema = new Schema<IPessoa>({
+    nome: String,
+    idade: Number,
+    profissao: String,
+});
+
+// const pessoa = model<IPessoa>
